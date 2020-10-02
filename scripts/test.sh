c89babaf68a1250994cc378533542e8cc069b8dc
Poppy5000 <71330865+Poppy5000@users.noreply.github.com>
2020-10-02 13:13:01 +0100

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,8 +4,8 @@
 sudo docker run -d -p 5000:5000 --name pytest localhost:8082/challenge_three
 
 # run pytest to initiate testing
-#pytest
-python3 app.py
+pytest
+#python3 app.py
 
 # pull down container
 sudo docker rm -f testing_pytest
