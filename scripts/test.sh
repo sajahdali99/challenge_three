7e0e43f7955c78bcd56aecce18dd0a2921f3191e
Poppy5000 <71330865+Poppy5000@users.noreply.github.com>
2020-10-02 12:39:56 +0100

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
 # create a container for test environment
-sudo docker build -t testing-pytest
-
 sudo docker run -d -p 5000:5000 --name testing_pytest testing_pytest
 
 # run pytest to initiate testing
