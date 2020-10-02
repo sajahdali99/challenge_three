685ad3b745f12ef8018506c04035b0d6a60a30a6
Poppy5000 <71330865+Poppy5000@users.noreply.github.com>
2020-10-02 14:07:35 +0100

    Update push_images.sh

--- scripts/push_images.sh
+++ scripts/push_images.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 #sudo docker push localhost:8082/challenge_three
-sudo docker tag version:5.0 localhost:8082/challenge_three
+sudo docker tag challenge_three:1.0 localhost:8082/challenge_three
 sudo docker push localhost:8082/challenge_three
